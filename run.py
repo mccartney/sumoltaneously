2c357aab306c26623095bc07b38a4fb6a11f0f50
Greg Oledzki <goledzki@sumologic.com>
2024-11-07 13:31:41 +0100

    Support for actual results of the query

--- run.py
+++ run.py
@@ -12,11 +12,11 @@ sumo = SumoLogic(os.environ["SUMO_ACCESS_ID"], os.environ["SUMO_ACCESS_KEY"], os
 with open("query.txt", "r") as query_file:
   query = query_file.read()
 
-duration = 160 * 60 * 1000
-startTimeStep = 30 * 60 * 1000
+duration = 24 * 60 * 60 * 1000
+startTimeStep = 10 * 24 * 60 * 60 * 1000
 
-firstStartTime = 1574636400000
-lastStartTime = 1575241200000
+firstStartTime = 1704067200000
+lastStartTime = 1730419200000
 
 runCount = int((lastStartTime - firstStartTime) / startTimeStep)
 
@@ -33,8 +33,7 @@ waitingForResults = {}
 maxConcurrentQueries = 10
 iterationsToRun = list(range(0, runCount))
 readyResults = {}
-
-print("The following iterations will need to be performed: %s" %(iterationsToRun))
+print("The following iterations will need to be performed: %s" %("\n".join(list(map(dateForIteration, iterationsToRun)))))
 
 while iterationsToRun or waitingForResults:
   # check already started runs
@@ -43,7 +42,7 @@ while iterationsToRun or waitingForResults:
     status = sumo.search_job_status(waiting)
     if status['state'] == 'DONE GATHERING RESULTS':
       print("Iteration %d ready, saving results" % (iteration))
-      readyResults[iteration] = status
+      readyResults[iteration] = (status, sumo.search_job_records(waiting, limit =1))
       iterationsToBeRemoved.append(iteration)
   for iteration in iterationsToBeRemoved: del(waitingForResults[iteration])
   
@@ -59,8 +58,12 @@ while iterationsToRun or waitingForResults:
 print("The results are ready: %s" % (len(readyResults)))
 print("Non-zero results: ")
 for iteration, result in sorted(readyResults.items()):
-  if result['recordCount'] > 0:
-    print("%4d %20s -> resultCount: %d" %(iteration, 
+  if result[0]['recordCount'] > 0:
+    fields = result[1]["fields"]
+    firstValues = {field["name"]:result[1]["records"][0]["map"][field["name"]] for field in fields}
+    print("%4d %20s -> resultCount: %d, 1st result: %s" %(iteration,
       dateForIteration(iteration),
-      result['recordCount']))
+      result[0]['recordCount'],
+      firstValues,
+      ))
     
